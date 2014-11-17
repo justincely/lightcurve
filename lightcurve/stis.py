ed02ed83c70f2c0c4f60d6f9aa207beb608a1a58
Justin Ely <ely@stsci.edu>
2014-11-17 16:22:28 -0500

    running, but incomplete, stis_corrtag function

--- lightcurve/stis.py
+++ lightcurve/stis.py
@@ -5,13 +5,18 @@ Holder of Space Telescope Imaging Spectrograph (STIS) classes and utilities
 
 from __future__ import print_function
 
-from astropy.io import fits as pyfits
 import os
 import numpy as np
+import scipy
 from scipy.interpolate import interp1d
+from datetime import datetime
+import astropy
+from astropy.io import fits as pyfits
 
 from .lightcurve import LightCurve
 from .utils import expand_refname
+from .stis_cal import calculate_epsilon
+from .version import version as  __version__
 
 #-------------------------------------------------------------------------------
 
@@ -96,7 +101,58 @@ def stis_corrtag(tagfile):
 
     """
 
-    eps_column = epsilon(tagfile)
+    with pyfits.open(tagfile, 'readonly') as hdu:
+        n_events = len(hdu[1].data['TIME'])
+
+        time_data = hdu[1].data['TIME']
+
+        #-- Y data is same in raw and corrected coordinates
+        rawx_data = hdu[1].data['DETAXIS1']
+        rawy_data = hdu[1].data['AXIS2']
+        xcorr_data = hdu[1].data['AXIS1']
+        ycorr_data = hdu[1].data['AXIS2']
+
+        #-- copy over the primary
+        header0 = hdu[0].header.copy()
+        header1 = hdu[1].header.copy()
+
+    eps_data = epsilon(tagfile)
+    wave_data = np.ones(n_events)
+    dq_data = np.ones(n_events)
+
+    #-- Writeout corrtag file
+    hdu_out = pyfits.HDUList(pyfits.PrimaryHDU())
+
+    #hdu_out[0].header = header0
+
+    hdu_out[0].header['GEN_DATE'] = (str(datetime.now()), 'Creation Date')
+    hdu_out[0].header['LC_VER'] = (__version__, 'lightcurve version used')
+    hdu_out[0].header['AP_VER'] = (astropy.__version__, 'Astropy version used')
+    hdu_out[0].header['NP_VER'] = (np.__version__, 'Numpy version used')
+    hdu_out[0].header['SP_VER'] = (scipy.__version__, 'Scipy version used')
+
+    time_col = pyfits.Column('time', 'D', 'second', array=time_data)
+    rawx_col = pyfits.Column('rawx', 'D', 'pixel', array=rawx_data)
+    rawy_col = pyfits.Column('rawy', 'D', 'pixel', array=rawy_data)
+    xcorr_col = pyfits.Column('xcorr', 'D', 'pixel', array=xcorr_data)
+    ycorr_col = pyfits.Column('ycorr', 'D', 'pixel', array=ycorr_data)
+    epsilon_col = pyfits.Column('epsilon', 'D', '', array=eps_data)
+    wave_col = pyfits.Column('wavelength', 'D', 'MJD', array=wave_data)
+    dq_col = pyfits.Column('dq', 'D', 'MJD', array=dq_data)
+
+    tab = pyfits.new_table([time_col,
+                            rawx_col,
+                            rawy_col,
+                            xcorr_col,
+                            ycorr_col,
+                            epsilon_col,
+                            wave_col,
+                            dq_col])
+    hdu_out.append(tab)
+
+    #hdu_out[1].header = header1
+
+    hdu_out.writeto(tagfile.replace('_tag.fits', '_corrtag.fits'), clobber=True)
 
 #-------------------------------------------------------------------------------
 
@@ -124,10 +180,9 @@ def epsilon(tagfile):
             reffile = expand_refname(hdu[0].header[ref_flat])
 
             with pyfits.open(reffile) as image:
-                for i, (x, y) in enumerate(zip(hdu[1].data['AXIS1'],
-                                               hdu[1].data['AXIS2'])):
-                    #--indexing is 1 off
-                    epsilon[i] *= image[1].data[x-1, y-1]
-
+                #--indexing is 1 off
+                epsilon *= calculate_epsilon(image[1].data,
+                                             hdu[1].data['AXIS1'] - 1,
+                                             hdu[1].data['AXIS2'] - 1)
 
     return epsilon
