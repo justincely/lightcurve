fffd9994f155e3de8262d78b7c941939c10eb545
Justin Ely <ely@stsci.edu>
2014-11-17 15:35:06 -0500

    initial version of stis calibration functions in c++

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
 import os
 import glob
+
+stis_cal_module = Extension('lightcurve/stis_cal',
+                            sources = ['lightcurve/stis_cal.cc'],
+                            extra_link_args=[ '-pthread',
+                                             '-shared',
+                                             '-lm']
+                             )
 
 setup(
     name = 'lightcurve',
     url = 'http://justincely.github.io/lightcurve/',
-    version = '0.2.1', 
+    version = '0.2.1',
     description = 'Create lightcurves from HST/COS data',
     author = 'Justin Ely',
     author_email = 'ely@stsci.edu',
@@ -18,6 +25,6 @@ setup(
                    'Topic :: Software Development :: Libraries :: Python Modules'],
     packages = ['lightcurve'],
     requires = ['numpy','scipy','astropy'],
-    scripts =  ['scripts/lightcurve']
+    scripts =  ['scripts/lightcurve'],
+    ext_modules = [stis_cal_module]
     )
-
